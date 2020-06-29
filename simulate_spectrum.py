c7aba6d54e8e96fe5de41ee765b2846f25d11535
nickill <nicholas.killerby-smith@anu.edu.au>
2020-06-30 04:41:45 +1000

    added functionality for inserting the desired velocity axis as an array, code is very untidy and needs streamlining. Esp. re: vellen and length being semi-redundant

--- simulate_spectrum.py
+++ simulate_spectrum.py
@@ -14,7 +14,9 @@ import gausspy.gausspy.gp as gp
 from gausspyplus.utils.gaussian_functions import gaussian, combined_gaussian
 
 def make_vel_ax(vel0=None,delvel=None,vellen=None,header=None,kms=True):
-    '''vel0, delvel and vellen values given in km/s'''
+    '''vel0, delvel and vellen values given in km/s. Because its supplied in km/s 
+    float values will result in rounding errors in the output. 
+    Could change it to be in m/s in the future to alleviate this issue if necessary'''
     if header != None:
         #x is in kms
         x=np.divide([header['CRVAL3']+k*header['CDELT3'] for k in range(header['NAXIS3'])],1000)
@@ -31,6 +33,7 @@ def gaussian(length,amplitude,width,position):
     return amplitude*np.exp(-0.5*((length-position)**2/(width/(2*np.sqrt(2*np.log(2))))**2))
 
 def sumgaussians(length, *args): 
+    '''comps should be in format amp,width,pos'''
     y=0
     for i in range(int(len(args)/3)): ###previously did range (1,int(len(...))) not sure if that was just an error left over from a previous indexing attempt
         y+=gaussian(length,args[0+(3*i)],args[1+(3*i)],args[2+(3*i)])
@@ -51,18 +54,16 @@ def sumgaussians_lmfit(args, x, data=None):
         return y
     return y - data
 
-def simulate_comp(length, fwhm, pos, header=None, tau_0=None,ts_0=None,vmin=-30,vmax=30):
-    '''simulates a gaussian component when given two of the three above values.'''
+def simulate_comp(length, fwhm, pos, header=None, tau_0=None,ts_0=None,vel0=-30,delvel=0.1,vellen=600,vel_ax=None):
+    '''simulates a gaussian component when given two of the three above values. vellen and length are redundant and don't interact properly'''
     
-    #centres the spectrum around 0km/s
-    
-    if header != None:
-        length = make_vel_ax
-    else:
-        length=np.linspace(vmin,vmax,length)
-
-
-    
+    #creates velocity axis from header
+    if header is not None:
+        length = make_vel_ax(header=header)
+    if vel_ax is not None:
+        length=vel_ax
+    else: #creates custom velocity axis, currently uses length instead of vellen
+        length=make_vel_ax(vel0=vel0,delvel=delvel,vellen=length)
 
     Ts=ts_0
     tau=gaussian(length,tau_0,fwhm,pos)
@@ -70,20 +71,26 @@ def simulate_comp(length, fwhm, pos, header=None, tau_0=None,ts_0=None,vmin=-30,
 
     return Tb, length
 
-def simulate_spec(length,*comps,tb_noise=0, tau_noise=0,vmin=-30,vmax=30):
+def simulate_spec(length,*comps,tb_noise=0, tau_noise=0,vel0=-30,delvel=0.5,vellen=600,vel_ax=None):
     '''First component has no opacity from other components and is physically first in the LOS. 
     Second component inputted will have the first blocking it and so on.
     Component should have format (fwhm,pos,Ts,Tau)
-    Noise is in std devs and hence in the units of the spectrum
+    Noise is in std devs and hence in the units of the spectrum.
+
+    vellen and length are redundant and don't interact properly
     '''
     #record the input components
     inputcomps=[i for i in comps]
     
-    #establish the velocity space
-    gausslen=np.linspace(vmin,vmax,length)
+    #establish the velocity space, currently uses length instead of vellen
+    if vel_ax is not None:
+        gausslen = vel_ax
+    else:
+        gausslen=make_vel_ax(vel0=vel0,delvel=delvel,vellen=length)
     
     #define the first component
-    comp1, comp1len = simulate_comp(length,comps[0][0],comps[0][1],ts_0=comps[0][2],tau_0=comps[0][3])
+    comp1, comp1len = simulate_comp(length,comps[0][0],comps[0][1],ts_0=comps[0][2],tau_0=comps[0][3],
+    vel0=vel0,delvel=delvel,vellen=length,vel_ax=vel_ax)
     
     
     #establish the spectra
@@ -95,7 +102,8 @@ def simulate_spec(length,*comps,tb_noise=0, tau_noise=0,vmin=-30,vmax=30):
     
     for i in range(1,len(comps)):
         #take the ith component beginning with the second comp
-        newcomp, newcomplen = simulate_comp(length,comps[i][0],comps[i][1],ts_0=comps[i][2],tau_0=comps[i][3])
+        newcomp, newcomplen = simulate_comp(length,comps[i][0],comps[i][1],ts_0=comps[i][2],tau_0=comps[i][3],
+        vel0=vel0,delvel=delvel,vellen=length,vel_ax=vel_ax)
        
         #add to the opacity spectrum the i-1th component
         sumtaus+=gaussian(gausslen,comps[i-1][3],comps[i-1][0],comps[i-1][1])
